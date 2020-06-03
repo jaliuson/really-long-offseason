6c4a427d463214738abe760f77054c2b1dffc52e
Jason Liu <jliu2383@uwo.ca>
2020-06-03 19:51:34 -0400

    Updates

---
+++ precache-manifest.68a5f9e669ac4590a90d89a431c9af41.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e0bc2259efa72ddd64b2a2164fcd1d4",
+    "url": "/really-long-offseason/index.html"
+  },
+  {
+    "revision": "101c873fcc9c8cf464cc",
+    "url": "/really-long-offseason/static/css/main.7473d8cb.chunk.css"
+  },
+  {
+    "revision": "570e15aa267ff02b8e9a",
+    "url": "/really-long-offseason/static/js/2.28b7df85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/really-long-offseason/static/js/2.28b7df85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "101c873fcc9c8cf464cc",
+    "url": "/really-long-offseason/static/js/main.f9ffdcdf.chunk.js"
+  },
+  {
+    "revision": "7d58f31e0678207ccce3",
+    "url": "/really-long-offseason/static/js/runtime-main.b6623e95.js"
+  },
+  {
+    "revision": "28a651c6729247f029d6ab512576d217",
+    "url": "/really-long-offseason/static/media/logo192.28a651c6.png"
+  },
+  {
+    "revision": "1c647910f1152cb3e40d6cccfe5fa26c",
+    "url": "/really-long-offseason/static/media/lowpoly.1c647910.png"
+  }
+]);
